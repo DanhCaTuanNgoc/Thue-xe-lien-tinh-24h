ba869bc64f84362a2317bb8fe800d35116391d20
Nguyễn Ngọc Tuấn <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2025-08-07 20:47:04 +0700

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,23 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <html lang="vi">
+         <head>
+            {/* Google tag (gtag.js) */}
+            <script
+               async
+               src="https://www.googletagmanager.com/gtag/js?id=AW-17393577943"
+            ></script>
+            <script
+               dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-17393577943');
+                  `,
+               }}
+            />
+         </head>
          <body className={`${inter.className} overflow-x-hidden overscroll-none`}>
             <ClientLayoutShell>{children}</ClientLayoutShell>
          </body>
